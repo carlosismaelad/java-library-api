d8a1bc69e50410a61c41377e9501b60f10629687
Carlos Dourado <carlosismaelad@outlook.com>
2025-04-08 22:02:59 -0300

    mapping livro entity

---
+++ src/main/java/com/carlosdourado/libraryapi/model/GeneroLivro.java
@@ -0,0 +1,10 @@
+package com.carlosdourado.libraryapi.model;
+
+public enum GeneroLivro {
+    FICCAO,
+    FANTASIA,
+    MISTERIO,
+    ROMANCE,
+    BIOGRAFIA,
+    CIENCIA
+}
---
+++ src/main/java/com/carlosdourado/libraryapi/model/Livro.java
@@ -0,0 +1,41 @@
+package com.carlosdourado.libraryapi.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@Table(name = "livro")
+@Data
+public class Livro {
+
+    @Id
+    @Column(name = "id", nullable = false)
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "isbn", length = 20, nullable = false)
+    private String isbn;
+
+    @Column(name = "titulo", length = 150, nullable = false)
+    private String titulo;
+
+    @Column(name = "data_publicacao", nullable = false)
+    private LocalDate dataPublicacao;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "genero", length = 30, nullable = false)
+    private GeneroLivro genero;
+
+    @Column(name = "preco", precision = 18, scale = 2)
+    private Double preco;
+
+    @ManyToOne
+    @JoinColumn(name = "id_autor")
+    private Autor autor;
+
+}
